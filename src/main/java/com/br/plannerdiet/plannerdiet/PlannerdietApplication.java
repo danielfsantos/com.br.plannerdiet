80c0b57e9ee67068cafad9a815d440ded19e4280
Daniel Fernandes <danielfsantos56@gmail.com>
2022-01-26 19:54:15 -0300

    Initial Commit

---
+++ src/main/java/com/br/plannerdiet/plannerdiet/PlannerdietApplication.java
@@ -0,0 +1,13 @@
+package com.br.plannerdiet.plannerdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerdietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerdietApplication.class, args);
+	}
+
+}
